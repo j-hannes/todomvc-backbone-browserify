fc02062e1033d5b53a5dd442b6375caf76a27385
Johannes Erber <Hannes_E@gmx.de>
2014-03-19 12:11:21 +0100

    Add new todos entered in input field

--- src/views/app-view.js
+++ src/views/app-view.js
@@ -10,6 +10,10 @@ var TodoView = require('../views/todo-view')
 // ----------------------------------------------------------------------------
 // AppView
 var AppView = Backbone.View.extend({
+  events: {
+    'keypress #new-todo': 'createOnEnter'
+  },
+
   initialize: function() {
     this.listenTo(this.collection, 'add', this.addOne)
   },
@@ -18,6 +22,17 @@ var AppView = Backbone.View.extend({
     var view = new TodoView({model: todo})
     this.$('#todo-list').append(view.render().el)
   },
+
+  createOnEnter: function(e) {
+    if (e.which === 13) {
+      var input = this.$('#new-todo')
+      var title = input.val().trim()
+      if (title) {
+        this.collection.add({title: title})
+        input.val('')
+      }
+    }
+  }
 })
 
 
